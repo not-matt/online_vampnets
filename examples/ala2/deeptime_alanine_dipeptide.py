37c53da704cc29a08b91211d27d29f0b674d289e
Matthew Bowley <m.bowley98@gmail.com>
2022-03-17 14:21:58 +0000

    Python script

---
+++ examples/ala2/deeptime_alanine_dipeptide.py
@@ -0,0 +1,194 @@
+# %% [markdown]
+# # Introduction 
+# 
+# This is a copy of the  example given on the Deeptime documentation website [here](https://deeptime-ml.github.io/latest/notebooks/examples/ala2-example.html).  Run this example first before trying any of the experimental code to make sure everting is working as it should! 
+
+# %%
+from copy import deepcopy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.nn as nn
+import mdshare  
+
+from deeptime.util.types import to_dataset
+from tqdm.notebook import tqdm  # progress bar
+from deeptime.decomposition.deep import VAMPNet
+from torch.utils.data import DataLoader
+
+
+# %% [markdown]
+# 
+
+# %%
+assert torch.cuda.is_available(), "Need a GPU with CUDA" 
+device = torch.device("cuda")
+torch.backends.cudnn.benchmark = True
+
+# %% [markdown]
+# ## Globals
+
+# %%
+data_dir = "/home/matthewb/online_vampnets/data"
+data_dir = "/home/mbowley/ANI-Peptides/outputs/likepaper_tests/aaa_solvated_ani2x_0.5fs_noconstraints_10ns_likepaper/trajectory_split_134011_170322"
+lag_time = 1
+
+
+# %%
+
+# ala_coords_file = mdshare.fetch(
+#     "alanine-dipeptide-3x250ns-heavy-atom-positions.npz", working_directory=data_dir
+# )
+
+# dihedral_file = mdshare.fetch(
+#     "alanine-dipeptide-3x250ns-backbone-dihedrals.npz", working_directory=data_dir
+# )
+
+file_coords = os.path.join(data_dir, "coords_archive.npz")
+with np.load(file_coords) as fh:
+    data_coords = fh["arr_0"].astype(np.float32)
+
+file_dihedral = os.path.join(data_dir, "dihedrals_archive.npz")
+with np.load(file_dihedral) as fh:
+    data_dihedral = fh["arr_0"].astype(np.float32)
+
+# %%
+f, ax = plt.subplots(1, 1)
+
+x = data_dihedral[:, 0, :].flat
+y = data_dihedral[:, 1, :].flat
+hb = ax.hexbin(x, y, mincnt=5)
+ax.set_aspect('equal')
+cb = f.colorbar(hb, ax=ax)
+cb.set_label('# of frames in bin')
+
+# %% [markdown]
+# The following function splits up each trajectory in data into pairs (X_t, X_t+lagtime) and then puts all pairs from each trajectory into a single generator. 
+# 
+# So for lagtime = 1: 
+#  - There are three trajectories. 
+#  - Each trajectory is 250000 x 30 (30 being the number of coordinates)
+#  - So there are 249999 time lagged pairs for each trajectory. 
+#  - in total then there are 749997 time-lagged pairs. 
+#  - The dataset will have length 749997, each item will be a tuple of length 2 (Xt, Xt+1), and each tuple item will have length 30
+# 
+
+# %%
+print(data_coords.shape)
+dataset = to_dataset(data=data_coords, lagtime=1)
+print(len(dataset), len(dataset[0]), len(dataset[0][0]))
+
+# %%
+n_coords = data_coords.shape[1]
+n_val = int(len(dataset)*.3)
+train_data, val_data = torch.utils.data.random_split(dataset, [len(dataset) - n_val, n_val])
+
+
+# %%
+lobe = nn.Sequential(
+    nn.BatchNorm1d(data_coords.shape[1]),
+    nn.Linear(data_coords.shape[1], n_coords), nn.ELU(),
+    nn.Linear(n_coords, 100), nn.ELU(),
+    nn.Linear(100, 100), nn.ELU(),
+    nn.Linear(100, 100), nn.ELU(),
+    nn.Linear(100, 100), nn.ELU(),
+    nn.Linear(100, 100), nn.ELU(),
+    nn.Linear(100, 100), nn.ELU(),
+    nn.Linear(100, n_coords), nn.ELU(),
+    nn.Linear(n_coords, 6),
+    nn.Softmax(dim=1)  # obtain fuzzy probability distribution over output states
+)
+
+lobe_timelagged = deepcopy(lobe).to(device=device)
+lobe = lobe.to(device=device)
+print(lobe)
+
+# %%
+vampnet = VAMPNet(lobe=lobe, lobe_timelagged=lobe_timelagged, learning_rate=5e-4, device=device)
+
+# %%
+loader_train = DataLoader(train_data, batch_size=10000, shuffle=True)
+loader_val = DataLoader(val_data, batch_size=len(val_data), shuffle=False)
+
+# %%
+model = vampnet.fit(loader_train, n_epochs=30,
+                    validation_loader=loader_val).fetch_model()
+
+# %%
+plt.loglog(*vampnet.train_scores.T, label='training')
+plt.loglog(*vampnet.validation_scores.T, label='validation')
+plt.xlabel('step')
+plt.ylabel('score')
+plt.legend();
+
+
+
+# %%
+# state_probabilities = model.transform(data_coords.reshape(-1, data_coords.shape[1]))
+state_probabilities = model.transform(data_coords)
+f, axes = plt.subplots(3, 2, figsize=(12, 16))
+for i, ax in enumerate(axes.flatten()):
+    ax.scatter(*data_dihedral[:, :, 0].T, c=state_probabilities[..., i][:])
+    ax.set_title(f'State {i+1}')
+
+# %%
+state_probabilities = model.transform(data_coords)
+for ix, (mini, maxi) in enumerate(zip(np.min(state_probabilities, axis=0),
+                                      np.max(state_probabilities, axis=0))):
+    print(f"State {ix+1}: [{mini}, {maxi}]")
+
+# %%
+assignments = state_probabilities.argmax(1)
+plt.scatter(*data_dihedral[:, :, 0].T, c=assignments, s=5, alpha=.2)
+plt.title('Transformed state assignments');
+
+
+
+# %%
+from deeptime.decomposition import VAMP
+
+vamp_estimator = VAMP(lagtime=10, observable_transform=model)
+vamp = vamp_estimator.fit(data_coords).fetch_model()
+print(f"VAMP-2 score under lag 10: s = {vamp.score(2).round(2)}")
+
+
+
+# %%
+lagtimes = np.arange(1, 201, dtype=np.int32)
+timescales = []
+for lag in lagtimes:
+    ts = VAMP(lagtime=lag, observable_transform=model).fit(data_coords).fetch_model().timescales(k=5)
+    timescales.append(ts)
+
+f, ax = plt.subplots(1, 1)
+ax.semilogy(lagtimes, timescales)
+ax.set_xlabel('lagtime')
+ax.set_ylabel('timescale / step')
+ax.fill_between(lagtimes, ax.get_ylim()[0]*np.ones(len(lagtimes)), lagtimes, alpha=0.5, color='grey')
+
+# %%
+vamp_estimator = VAMP(lagtime=1, observable_transform=model)
+vamp = vamp_estimator.fit(data_coords).fetch_model()
+
+validator = vamp_estimator.chapman_kolmogorov_validator(mlags=20)
+cktest = validator.fit(data_coords, n_jobs=1).fetch_model()
+
+# %%
+n_states = len(vamp.singular_values)
+
+tau = cktest.lagtimes[1]
+steps = len(cktest.lagtimes)
+fig, ax = plt.subplots(n_states, n_states, sharex=True, sharey=True, constrained_layout=True)
+for i in range(n_states):
+    for j in range(n_states):
+        pred = ax[i][j].plot(cktest.lagtimes, cktest.predictions[:, i, j], color='b')
+        est = ax[i][j].plot(cktest.lagtimes, cktest.estimates[:, i, j], color='r', linestyle='--')
+        ax[i][j].set_title(str(i+1)+ '->' +str(j+1),
+                                       fontsize='small')
+ax[0][0].set_ylim((-0.1,1.1));
+ax[0][0].set_xlim((0, steps*tau));
+ax[0][0].axes.get_xaxis().set_ticks(np.round(np.linspace(0, steps*tau, 3)));
+fig.legend([pred[0], est[0]], ["Predictions", "Estimates"], 'lower center', ncol=2,
+           bbox_to_anchor=(0.5, -0.1));
